dbb9d725d565378cf5d7bef7c501ea17a773f25a
Nesrine <nesrinakon@gmail.com>
2017-11-11 15:23:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 
 
-function init(e) {
+function init() {
   let index = 0
   document.body.addEventListener('keydown', function(e){
     const keyCode = parseInt(e.detail || e.which)
@@ -16,5 +16,5 @@ function init(e) {
     else{
       index=0
     }
-}
+  }
 }
