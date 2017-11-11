46bc1e10d9b5a3ea2450d9557d85089250815a17
Nesrine <nesrinakon@gmail.com>
2017-11-11 14:59:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0
 document.body.addEventListener('keydown', init)
 
-function init() {
-
-    cont keyCode = parseInt(e.detail || e.which)
+function init(e) {
+    const keyCode = parseInt(e.detail || e.which)
     if(keyCode === code[index]){
       index++
       if(index === code.length){
